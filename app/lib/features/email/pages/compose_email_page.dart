e86402990233322a95264b8fde6bcd95f9d5809e
DivyanshArya09 <aryadivyansh@gmail.com>
2024-03-31 10:14:36 +0530

    updates

---
+++ app/lib/features/email/pages/compose_email_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class ComPoseEmailPage extends StatefulWidget {
+  const ComPoseEmailPage({super.key});
+
+  @override
+  State<ComPoseEmailPage> createState() => _ComPoseEmailPageState();
+}
+
+class _ComPoseEmailPageState extends State<ComPoseEmailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView();
+  }
+}
