3448944d895ebbff6e6478632febb4222cb6997b
DivyanshArya09 <aryadivyansh@gmail.com>
2024-04-10 00:34:02 +0530

    added service locator

--- app/lib/core/helpers/helper.dart
+++ app/lib/core/helpers/helper.dart
@@ -1,27 +1,28 @@
 import 'dart:convert';
 
 import 'package:app/core/errors/failures.dart';
+import 'package:app/injection_container.dart';
 import 'package:dartz/dartz.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SharedPreferencesHelper {
   static Future<void> saveString(String value) async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final prefs = await sl<SharedPreferences>();
     await prefs.setString('uid', value);
   }
 
   static Future<String?> getString() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final prefs = await sl<SharedPreferences>();
     return prefs.getString('uid');
   }
 
   static Future<void> setDestrict(String district) async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final prefs = await sl<SharedPreferences>();
     await prefs.setString('currentlocation', district);
   }
 
   static Future<void> setLocation(Map<String, dynamic> location) async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final prefs = await sl<SharedPreferences>();
     String json = jsonEncode(location);
     await prefs.setString('currentlocation', json);
     // return prefs
